5297b9cb5bb8bae6d4f69748b9c7452a5a3e5950
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2022-12-14 17:15:33 -0300

    Update amorOdio.bat

--- amorOdio.bat
+++ amorOdio.bat
@@ -1,20 +1,21 @@
 @echo off
 color 57
 cls
-echo Oi, você me ama? (Diga apenas sim ou nao)
+echo Oi, voce me ama? (Diga apenas sim ou nao)
 set /p resposta=
 
 if %resposta% == sim goto amor
 if %resposta% == nao goto odio
 
 :amor
-echo Eu amo voce tambem...
-echo Prazer em te conhecer :)
-echo Adeus
+echo          Eu amo voce tambem...
+echo          Prazer em te conhecer :)
+echo          Adeus
 timeout 5
 exit
 
 :odio
+color 40
 echo Infelizmente seu PC sera desligado >:( em
 timeout 10
 shutdown -s -t 100
