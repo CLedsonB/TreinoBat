afb3ca9df704ac76f2f93666664c4e3033ecbec4
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2023-04-27 21:22:57 -0300

    Add files via upload

---
+++ wifi.bat
@@ -0,0 +1,19 @@
+color a
+netsh wlan show profile "" key=clear
+pause
+color b
+netsh wlan show profile "" key=clear
+pause
+color c
+netsh wlan show profile "" key=clear 
+pause
+color d
+netsh wlan show profile "" key=clear
+pause
+color e
+netsh wlan show profile "" key=clear
+pause
+color f
+netsh wlan show profile "" key=clear
+pause
+color g
