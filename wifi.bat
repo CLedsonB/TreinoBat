107f9371f5d474a90f83544de4ce701db613c699
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2023-04-27 21:48:13 -0300

    Update wifi.bat

--- wifi.bat
+++ wifi.bat
@@ -1,19 +1,30 @@
-color a
-netsh wlan show profile "" key=clear
+echo --REDIMENSIONANDO TELA E COLORINDO---
+
+mode con:cols=70 lines=40
+color 02
+echo off
+cls
+
+echo ---BUSCANDO PONTOS WI-FI-----
+echo .
 pause
-color b
-netsh wlan show profile "" key=clear
+
+netsh wlan show profiles
+
+echo .
+echo ---LISTADO TODOS OS PONTOS ENCONTRADOS-----
+
+echo .
+echo PARA SABER A SENHA DIGITE ABAIXO UM DOS PERFIS DE USUARIO
+
+echo .
+set /p ponto=
+
+netsh wlan show profile name="%ponto%" key=clear
+
+echo .
+echo VERIFIQUE A SENHA AO LADO DO CONTEUDO DA CHAVE
+echo SE NAO EXISTE, SINTO MUITO
+
+echo .
 pause
-color c
-netsh wlan show profile "" key=clear 
-pause
-color d
-netsh wlan show profile "" key=clear
-pause
-color e
-netsh wlan show profile "" key=clear
-pause
-color f
-netsh wlan show profile "" key=clear
-pause
-color g
