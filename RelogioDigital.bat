2f27fb559c614c219cad9949010b3dc7070747ce
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2023-05-05 21:52:33 -0300

    Update RelogioDigital.bat

--- RelogioDigital.bat
+++ RelogioDigital.bat
@@ -1,13 +1,13 @@
 @echo off
 title Relogio Digital
-@mode con cols=30 lines=7
+@mode con cols=40 lines=7
 color 02
 :main
 cls
 echo.
-echo Horas: %time%
+echo.   Horas: %time%
 echo.
-echo Data: %date%
+echo.   Data: %date%
 echo.
 ping -n 2 0.0.0.0 > nul
 goto main
