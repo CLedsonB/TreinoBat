f687883a4c17950047d3dcc66ff8a3ba96757242
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2023-05-05 21:33:22 -0300

    Update game.bat

--- game.bat
+++ game.bat
@@ -23,6 +23,8 @@ if %opc% == 1 goto INICIAR1
 if %opc% == 2 goto CREDITOS
 if %opc% == 3 goto SAIR
 
+______________________________________________
+
 :SAIR
 
 cls
@@ -39,6 +41,8 @@ echo.
 echo.
 pause
 exit /b
+
+______________________________________________
 
 :CREDITOS
 
@@ -64,6 +68,8 @@ set /p resposta1=.	Responda com (sim / nao) :
 if %resposta1% == sim goto VISITAR
 if %resposta1% == nao goto MENU
 
+______________________________________________
+
 :VISITAR
 set /p resposta2=.	Responda com (1 / 2): 
 
@@ -71,15 +77,17 @@ if %resposta2% == 1 start https://github.com/CLedsonB
 if %resposta2% == 2 start https://pt.wikihow.com/Criar-um-Jogo-com-o-CMD
 goto MENU
 
+______________________________________________
+
 :INICIAR1
 
-cls
-mode con:cols=80 lines=30
+mode con:cols=70 lines=30
 color 0B
+cls
 
 echo.
 echo.
-echo 		LUTE :-D
+echo 			PROLOGO  /_(-_-)_\
 echo.
 
 echo. 	Ai! minha nossa, os inimigos se aproximam
@@ -87,48 +95,119 @@ echo.	as pestes vem em grupo e estao armados ate os dentes
 echo.	mas eu boto fe em voce, lute guerreiro!
 echo.
 echo.	Gostaria de lutar ou fugir?
-set /p opc2 =.	Responda com (lutar / fugir) : 
-
-if %opc% == lutar goto LUTAR1
-if %opc% == fugir goto FUGIR1
-pause
-
-:LUTAR1
-
-cls
-mode con:cols=70 lines=30
-color 0B
-
 echo.
-echo.
-echo 		-------
-echo.
+set /p opc2=.	Responda com (lutar / fugir) : 
 
+if %opc2% == lutar goto LUTAR1
+if %opc2% == fugir goto FUGIR1
+
+______________________________________________
 
 :FUGIR1
 
+mode con:cols=60 lines=12
+color df
 cls
-title 
-mode con:cols=80 lines=30
-color 0B
 
 echo.
 echo.
 echo 		 (@_@)_/* SEU FRACO!!
 echo.
-echo.	Os fracos fogem para se tornarem fortes
+echo.	"Os fracos fogem para se tornarem fortes...."
+echo.
+echo.			"Autor Desconhecido - (2023)"
+echo.
+echo.
 pause
 goto INICIAR1
 
 
+______________________________________________
+
+:LUTAR1
+
+mode con:cols=50 lines=30
+color 4f
+cls
+
+echo.
+echo.
+echo 		LUTE :-D
+echo.
+echo.	Prepare-se para a batalha.
+echo.	Os inimigos partem pra cima 
+echo.	de ti com tudo!!
+echo.	
+echo.
+pause
+goto LUTAR_LOOP
+
+______________________________________________
+
+:LUTAR_LOOP
+
+echo.
+echo.		....Ataques....
+
+set /a num= %random%
+
+if %num% gtr 10 goto LUTAR_LOOP
+if %num% lss 5 goto LUTAR_LOOP
+
+if %num% == 5 goto PERDER_LUTA
+if %num% == 6 goto PERDER_LUTA
+if %num% == 7 goto PERDER_LUTA
+
+if %num% == 8 goto GANHAR_LUTA
+if %num% == 9 goto GANHAR_LUTA
+if %num% == 10 goto GANHAR_LUTA
 
 
+______________________________________________
 
+:PERDER_LUTA
 
+color 74
+mode con:cols=55 lines=12
+cls
 
+echo.
+echo.
+echo 		 :-/ VOCE FOI DERROTADO
+echo.
 
+echo.	Voce nao e forte o suficiente, meu nobre...
+echo.	Quer tentar de novo ?
+echo.
+set /p resposta3=.	Responda com (sim / nao) : 
 
+if %resposta3% == sim goto MENU
+if %resposta3% == nao goto SAIR
 
+______________________________________________
 
+:GANHAR_LUTA
 
+color 4e
+mode con:cols=55 lines=12
+cls
 
+echo.
+echo.
+echo.	""""""""""""""""""""""""""""""""
+echo.	"         *     *     *        "
+echo.	"       |\/\/\/\/\/\/\/\/|     "
+echo.	"       |... VENCEDOR ...|     "
+echo.	"       |________________|     "
+echo.	"                              "
+echo.	""""""""""""""""""""""""""""""""
+
+echo.	Em breve sera o novo rei, meu nobre...
+echo.	Quer tentar de novo ?
+echo.
+set /p resposta4=.	Responda com (sim / nao) : 
+
+if %resposta4% == sim goto LUTAR1
+if %resposta4% == nao goto SAIR
+
+______________________________________________
