b5058bf02f7d49bceb51f7579620eb1375eec40b
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2023-06-30 12:59:28 -0300

    Finish desligar.bat

--- desligar.bat
+++ desligar.bat
@@ -1,17 +1,55 @@
-echo --REDIMENSIONANDO TELA E COLORINDO---
-
-mode con:cols=60 lines=40
-color 02
-echo off
+@echo off
+mode con:cols=40 lines=30 
+color 0a
+chcp 65001
+title [DESLIGAR]
+setlocal
 cls
+echo.
+echo.	---DESLIGAR PC---
+echo.
+echo.	[1] Desligar
+echo.	[2] Reiniciar
+echo.	[3] Cancelar
+echo.
+echo.	Escolha uma das opções acima
+echo.
+choice /c 123 /n /m "--- Digite aqui : "
+cls
+echo.
 
-echo ---DESLIGAR O PC-----
-echo .
-echo --digite o tempo para desligar em segundos ou--
-echo --digite 0 para desligar imediatamente--
-echo .
-set /p tempo=
+if errorlevel 3 goto opc3
+if errorlevel 2 goto opc2
+if errorlevel 1 goto opc1
 
-echo .
-echo ---DESLIGANDO EM %tempo% segundos-----
-shutdown -s -t %tempo%
+:opc1
+echo Insira o tempo em seg. para desligar
+echo.
+set /p s= : 
+shutdown /s /f /t %s%
+color 47
+cls
+echo.
+echo. Restam %s% segundos para desligar
+pause
+exit
+
+:opc2
+echo Insira o tempo em seg. para reiniciar
+echo.
+set /p t= : 
+shutdown /r /t %t%
+color 60
+cls
+echo.
+echo. Restam %t% segundos para reiniciar
+pause
+exit
+
+:opc3
+shutdown /a
+echo.
+echo. Cancelamento efetuado com sucesso!
+echo.
+pause
+exit
